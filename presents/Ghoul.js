2fb7ba53054e0034b263befc2a90a77aa92ed54c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 11:08:09 -0400

    Create Ghoul.js

---
+++ presents/Ghoul.js
@@ -0,0 +1,29 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
+
+export default class Ghoul extends Mob {
+  constructor() {
+    super();
+    this.Name = "Ghoul";
+    this.Icon = "\"shade\""
+    
+    this.Str = 1;
+    this.Dex = 2;
+    this.Con = 0;
+    this.Int = -2;
+    this.Wis = 0;
+    this.Chr = -2;
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Bite", "2d6", "piercing", 2, 2),
+      new Weapon("Claw", "2d4", "slashing", 4, 2)
+    ];
+    
+    this.EquipWeapon = this.Weapons[0];
+    
+  }
+  
+}
