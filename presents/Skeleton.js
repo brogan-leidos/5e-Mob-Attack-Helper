f43f9e297aadf5cb9d62964ab3ded154c1399eb0
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 10:01:05 -0400

    Update Skeleton.js

--- presents/Skeleton.js
+++ presents/Skeleton.js
@@ -1,17 +1,22 @@
 import * from './Mob'
 import Weapon from './Weapon'
+import DamageRoll from './DamageRoll'
 
 export default class Skeleton extends Mob {
   constructor() {
     this.Str = 0;
-    this.Dex = 0;
-    this.Con = 0;
-    this.Int = 0;
-    this.Wis = 0;
-    this.Chr = 0;
+    this.Dex = 2;
+    this.Con = 2;
+    this.Int = -2;
+    this.Wis = -1;
+    this.Chr = -3;
+    
+    this.DamageRoll = new DamageRoll();
     
     this.Weapons = [
-      new Weapon("Shortsword", "1d6", "slashing", true),
+      new Weapon("Shortsword", "1d6", "slashing", 4, 2),
+      new Weapon("Shortbow", "1d6", "piercing", 4, 2),
+      new Weapon("Light Crossbow", "1d8", "piercing", 4, 2),
     ]
     
   }
