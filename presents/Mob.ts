b2b3fe5b8ecb2626009de1e71d150b53e46a0dfa
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 07:23:26 -0400

    Update Mob.ts

--- presents/Mob.ts
+++ presents/Mob.ts
@@ -15,49 +15,50 @@ class Mob {
     
     // Make an attack roll, modify by equip weapon
     function makeAttack(): any {
-        self.rollClass.attacker = self
-
-        attackRoll = random.randint(1,20)
+        this.rollClass.attacker = this
+        
+        //Math.floor(Math.random() * 10); //int 0 - 9
+        const attackRoll = Math.floor(Math.random() * 20 + 1);
         if attackRoll == 1:
-            self.rollClass.hitRoll = 1
+            this.rollClass.hitRoll = 1
             return 1
         if attackRoll == 20:
-            self.rollClass.hitRoll = "crit"
+            this.rollClass.hitRoll = "crit"
             return "crit"
         
-        self.rollClass.hitRoll = attackRoll + self.EquipWeapon.BonusToHit
-        return self.rollClass.hitRoll
+        this.rollClass.hitRoll = attackRoll + this.EquipWeapon.BonusToHit
+        return this.rollClass.hitRoll
     }
     
     // Make a strike using equip weapon
     function dealDamage(self): any {
-        splitString = self.EquipWeapon.DamageDie.split("d")
+        splitString = this.EquipWeapon.DamageDie.split("d")
         numAttacks = int(splitString[0])
         maxDamage = int(splitString[1])
         
         damageTotal = 0
         for i in range(0, numAttacks):
             damageTotal = damageTotal + random.randint(1, maxDamage)
-        damageTotal = damageTotal + self.EquipWeapon.BonusToDmg
-        self.rollClass.damageRoll = damageTotal
-        self.rollClass.damageDie = self.EquipWeapon.DamageDie
-        return self.rollClass
+        damageTotal = damageTotal + this.EquipWeapon.BonusToDmg
+        this.rollClass.damageRoll = damageTotal
+        this.rollClass.damageDie = this.EquipWeapon.DamageDie
+        return this.rollClass
     }
         
     // Crit using equip weapon!
     function dealCrit(self): any {
-        splitString = self.EquipWeapon.DamageDie.split("d")
+        splitString = this.EquipWeapon.DamageDie.split("d")
         numAttacks = int(splitString[0])
         maxDamage = int(splitString[1])
         
         damageTotal = 0
         for i in range(0, numAttacks*2):
             damageTotal = damageTotal + random.randint(1, maxDamage)
-        damageTotal = damageTotal + self.EquipWeapon.BonusToDmg
+        damageTotal = damageTotal + this.EquipWeapon.BonusToDmg
         
-        self.rollClass.damageRoll = damageTotal
-        self.rollClass.damageDie = self.EquipWeapon.DamageDie
-        self.rollClass.crit = True
+        this.rollClass.damageRoll = damageTotal
+        this.rollClass.damageDie = this.EquipWeapon.DamageDie
+        this.rollClass.crit = True
         return self.rollClass
     }
 }
