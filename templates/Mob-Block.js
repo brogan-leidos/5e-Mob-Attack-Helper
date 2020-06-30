12dfa3aa8d320d8d93a080affe9593d810420b1d
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 09:22:36 -0400

    Update Mob-Block.js

--- templates/Mob-Block.js
+++ templates/Mob-Block.js
@@ -1,6 +1,30 @@
 export function mobBlock() {
-  return `<div style="border: thin solid rgb(0, 0, 0)">
-            Test block
-          </div>`
+  return `<table style="border: thin solid #000000; width: 234px;">
+  <tbody>
+    <tr>
+      <td style="width: 63px;">Name:</td>
+      <td style="width: 155px;"><input id="mobName" type="text" /></td>
+    </tr>
+    <tr>
+      <td style="width: 63px;">Icon:</td>
+      <td style="width: 155px;"><select id="icons" name="Icons">
+        <option value="smile">😀</option>
+        <option value="skull">🧟&zwj;</option>
+        <option value="zombie">💀</option>
+        <option value="shade">👤</option>
+      </select>
+      </td>
+    </tr>
+    <tr>
+      <td style="width: 63px;">Weapon:</td>
+      <td style="width: 155px;"><input style="width: 30px;" type="text" />d<input style="width: 30px;" type="text" /></td>
+    </tr>
+    <tr>
+      <td>Number:</td>
+      <td><input type="number"</td>
+    </tr>
+  </tbody>
+</table>
+<br />`
 }
 
