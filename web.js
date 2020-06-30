05f4dfa187e72a242ea10a3dc8eeedb444159c12
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 09:30:25 -0400

    Update web.js

--- web.js
+++ web.js
@@ -2,17 +2,19 @@ import * as Mob from './presents/Mob.js'
 import {mobBlock} from './templates/Mob-Block.js'
 
 export default () => {
-    var numSkeletons = document.getElementById('numSkeletons');
-    var numZombies = document.getElementById('numZombies');
-    var numGhouls = document.getElementById('numGhouls');
+    var casterProficiency = document.getElementById('casterProficiency');
     var targetAc = document.getElementById('targetAc');
-    
-    var mobArray = []
-    
+        
     var mobBlockArea = document.getElementById('mobBlockArea');
     var infoArea = document.getElementById('infoArea'); // Debug info and roll info
 
     var goButton = document.getElementById('goButton');
+    var skeletonButton = document.getElementById('addSkeleton');
+    var zombieButton = document.getElementById('addZombie');
+    var ghoulButton = document.getElementById('addGhoul');
+
+    var mobArray = []
+
 
     var mobIncrement = 0;
     
@@ -22,5 +24,27 @@ export default () => {
         mobBlockArea.innerHTML.replace("FILLER", "Mob" + mobIncrement);
         mobIncrement++;
     });
+    
+     skeletonButton.addEventListener('click', () => {
+        infoArea.innerHTML = "Hello world!"
+        mobBlockArea.innerHTML += mobBlock()
+        mobBlockArea.innerHTML.replace("FILLER", "Mob" + mobIncrement);
+        mobIncrement++;
+    });
+
+    zombieButton.addEventListener('click', () => {
+        infoArea.innerHTML = "Hello world!"
+        mobBlockArea.innerHTML += mobBlock()
+        mobBlockArea.innerHTML.replace("FILLER", "Mob" + mobIncrement);
+        mobIncrement++;
+    });
+    
+    ghoulButton.addEventListener('click', () => {
+        infoArea.innerHTML = "Hello world!"
+        mobBlockArea.innerHTML += mobBlock()
+        mobBlockArea.innerHTML.replace("FILLER", "Mob" + mobIncrement);
+        mobIncrement++;
+    });    
+    
     
 };
