2819117c9e1708c6536eb3f473029c1eaf873163
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 14:02:48 -0400

    Update Mob.js

--- presents/Mob.js
+++ presents/Mob.js
@@ -24,45 +24,45 @@ export default class Mob {
     
     // Make an attack roll, modify by equip weapon
     makeAttack() {
-        this.rollClass.attacker = this
+        this.rollClass.attacker = this;
         
         //Math.floor(Math.random() * 10); //int 0 - 9
         var attackRoll = Math.floor(Math.random() * 20 + 1);
         if (attackRoll == 1) {
-            this.rollClass.hitRoll = 1
-            return 1
+            this.rollClass.hitRoll = 1;
+            return 1;
         }
         if (attackRoll == 20) {
-            this.rollClass.hitRoll = "crit"
-            return "crit"
+            this.rollClass.hitRoll = "crit";
+            return "crit";
         }
         
-        this.rollClass.hitRoll = attackRoll + this.EquipWeapon.BonusToHit
-        return this.rollClass.hitRoll
+        this.rollClass.hitRoll = attackRoll + parseInt(this.EquipWeapon.BonusToHit);
+        return this.rollClass.hitRoll;
     }
     
     // Make a strike using equip weapon
     dealDamage() {       
-        var damageTotal = 0
+        var damageTotal = 0;
         for(var i=0; i < this.EquipWeapon.NumDice; i++) {
-            damageTotal = damageTotal + Math.floor(Math.random() * this.EquipWeapon.DamageDie + 1) // 1 - maxdmg
+            damageTotal = damageTotal + Math.floor(Math.random() * this.EquipWeapon.DamageDie + 1); // 1 - maxdmg
         }
-        damageTotal = damageTotal + this.EquipWeapon.BonusToDmg
-        this.rollClass.damageRoll = damageTotal
-        this.rollClass.damageDie = this.EquipWeapon.DamageDie
-        return this.rollClass
+        damageTotal = damageTotal + this.EquipWeapon.BonusToDmg;
+        this.rollClass.damageRoll = damageTotal;
+        this.rollClass.damageDie = this.EquipWeapon.DamageDie;
+        return this.rollClass;
     }
         
     // Crit using equip weapon!
      dealCrit() {
-        var damageTotal = 0
+        var damageTotal = 0;
         for(var i=0; i < this.EquipWeapon.NumDice*2; i++) {
-            damageTotal = damageTotal + Math.floor(Math.random() * this.EquipWeapon.DamageDie + 1) // 1 - maxdmg
+            damageTotal = damageTotal + Math.floor(Math.random() * this.EquipWeapon.DamageDie + 1); // 1 - maxdmg
         }
-        damageTotal = damageTotal + this.EquipWeapon.BonusToDmg
-        this.rollClass.damageRoll = damageTotal
-        this.rollClass.damageDie = this.EquipWeapon.DamageDie
-        this.rollClass.crit = true
-        return this.rollClass
+        damageTotal = damageTotal + this.EquipWeapon.BonusToDmg;
+        this.rollClass.damageRoll = damageTotal;
+        this.rollClass.damageDie = this.EquipWeapon.DamageDie;
+        this.rollClass.crit = true;
+        return this.rollClass;
     }
 }
