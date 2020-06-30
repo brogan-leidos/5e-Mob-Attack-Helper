48add072f34677bad1125cb514030e0b5e2ac961
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-06-30 08:44:21 -0400

    Update DamageRoll.js

--- presents/DamageRoll.js
+++ presents/DamageRoll.js
@@ -1,7 +1,7 @@
 class DamageRoll {
-    var crit = false
-    var hitRoll = 0
-    var damageRoll = 0
-    var damageDie = 0
-    var attacker = null
+    var crit = false;
+    var hitRoll = 0;
+    var damageRoll = 0;
+    var damageDie = 0;
+    var attacker = null;
 }
