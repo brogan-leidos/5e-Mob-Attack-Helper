09bd58ee18a3d01799057a432b8305fe38a09b0c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-01 07:26:28 -0400

    Update utils.js

--- templates/utils.js
+++ templates/utils.js
@@ -1,3 +1,18 @@
-export function preserveHtml(html) {
-
+export function updateHtmlValues(html) {
+  
+  var numBlocks = blockArray.length;
+    
+    // Go though each creature block, spawn a number of mobs with those stats
+    for(var i=0;i < numBlocks;i++) {
+        var enabled = document.getElemenyById(blockArray[i].concat("-Enabled")).value;
+        var name = document.getElementById(blockArray[i].concat("-Name")).value;
+        var icon = document.getElementById(blockArray[i].concat("-Icon")).value;
+        var tohit = document.getElementById(blockArray[i].concat("-ToHit")).value;
+        var weapon = document.getElementById(blockArray[i].concat("-Weapon")).value;
+        var number = document.getElementById(blockArray[i].concat("-Number")).value;        
+        var advantage = document.getElementById(blockArray[i].concat("-Adv")).checked;
+        var disadvantage = document.getElementById(blockArray[i].concat("-Dis")).checked;
+      
+      
+    }
 }
