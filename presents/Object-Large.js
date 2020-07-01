1fcfe43bbf2c0033353bb99f8d17ce2a3476cc2b
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-01 15:04:23 -0400

    Update Object-Large.js

--- presents/Object-Large.js
+++ presents/Object-Large.js
@@ -1,1 +1,28 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
 
+export default class Object-Large extends Mob {
+  constructor() {
+    super();
+    this.Name = "Object-Large";
+    this.Icon = "\"robot\""
+    
+    this.Str = 2;
+    this.Dex = 0;
+    this.Con = 0;
+    this.Int = -4;
+    this.Wis = -4;
+    this.Chr = -5;
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Slam", 2, 10, "bludgeoning", 6, 2)
+    ];
+    
+    this.EquipWeapon = this.Weapons[0];
+    
+  }
+  
+}
