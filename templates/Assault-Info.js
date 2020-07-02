5307f2be57a718062d2eb2a7fc3863d8b43c2e39
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-02 13:21:56 -0400

    Create Assault-Info.js

---
+++ templates/Assault-Info.js
@@ -0,0 +1,15 @@
+export function assaultInfo() {
+  return `<div id="FILLER-BLOCK" >
+<table class="mobBlock">
+  <tbody>
+    <tr>
+      <td style="width:10px;"><button id="FILLER-BLOCK-Delete" style="color:red;" title="Delete mob">
+        &times;
+      </button></td>
+      <td style="width: 130px;">Name:</td>
+      <td style="width: 150px;"><input id="FILLER-BLOCK-Name" type="text" value="FILLER-NAME" /></td>
+    </tr>
+  </tbody>
+</table>
+</div>`
+}
