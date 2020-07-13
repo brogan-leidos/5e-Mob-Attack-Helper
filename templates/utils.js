bcb66ae45dfcf54a2939c55cc118e8b01c9214f6
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-13 07:35:19 -0400

    Update utils.js

--- templates/utils.js
+++ templates/utils.js
@@ -4,9 +4,9 @@ import Weapon from '../presents/Weapon.js'
 export function weaponsToHtml(weapons) {
   // weapons is a weapons array
   
-  htmlReturn = "";
+  var htmlReturn = "";
   for(var i=0; i < weapons.length; i++) {    
-    weapon = weapons[i];
+    var weapon = weapons[i];
     htmlReturn += `
       <option id="weapon.Name">
         weapon.Name
