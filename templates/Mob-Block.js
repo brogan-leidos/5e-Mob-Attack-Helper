d57b447ffab59e77a27878e5feba4f6149ba04af
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-13 07:45:35 -0400

    Update Mob-Block.js

--- templates/Mob-Block.js
+++ templates/Mob-Block.js
@@ -35,7 +35,7 @@ export function mobBlock() {
       <td><input id="FILLER-BLOCK-ToHit" type="number" value="FILLER-TOHIT" /></td>
     </tr>
     <tr>
-      <td style="width:10px">
+      <td style="width:30px">
         <select id="FILLER-BLOCK-Weapon-Select>
           <option value="custom">Custom</option>
           ADDITIONAL-WEAPONS
