5032eb7457cc8a20dfe0d16518695151b603e4fd
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-15 14:34:33 -0400

    Create Dretch.js

---
+++ presents/Dretch.js
@@ -0,0 +1,22 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
+
+export default class Dretch extends Mob {
+  constructor() {
+    super();
+    this.Name = "Dretch";
+    this.Icon = "\"devil\""   
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Claws", 2, 4, "slashing", 2, 0),
+      new Weapon("Bite", 1, 6, "piercing", 2, 0),
+    ];
+    
+    this.EquipWeapon = this.Weapons[0];
+    
+  }
+  
+}
