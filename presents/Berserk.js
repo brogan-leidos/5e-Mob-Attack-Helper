7b9c79574e091b70c54453fbd1fe62ecaf963461
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 07:42:34 -0400

    Create Berserk.js

---
+++ presents/Berserk.js
@@ -0,0 +1,21 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
+
+export default class Berserk extends Mob {
+  constructor() {
+    super();
+    this.Name = "Berserk";
+    this.Icon = "\"devil\""   
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Great Ax", 1, 12, "slashing", 5, 3)
+    ];
+    
+    this.EquipWeapon = this.Weapons[0];
+    
+  }
+  
+}
