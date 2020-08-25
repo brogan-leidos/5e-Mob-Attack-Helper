5c16d57e4df38a57f3598df840b8e1de2556a95b
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-25 09:30:18 -0400

    Update Discovery-Template.js

--- templates/Discovery-Template.js
+++ templates/Discovery-Template.js
@@ -1,20 +1,22 @@
 export function discoveryTemplate(roll) {  
   
-  return `
-<table class="discoveryBlock">
-  <tbody>
-    <tr>
-       ${roll}
-    </tr>
-    <tr>
-    <td>
-      <button id="hitButton">Hit!</button>
-    </td>
-    <td>
-      <button id="missButton">Missed</button>
-    </td>
-    </tr>
-  </tbody>
-</table>
-`
+  var temp = `
+  <table class="discoveryBlock">
+    <tbody>
+      <tr>
+         ${roll}
+      </tr>
+      <tr>
+      <td>
+        <button id="hitButton">Hit!</button>
+      </td>
+      <td>
+        <button id="missButton">Missed</button>
+      </td>
+      </tr>
+    </tbody>
+  </table>
+  `
+  
+  return temp;
 }
