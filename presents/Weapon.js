01ba39162cd2b5a41575ad75e8116fd96b482136
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-09 09:15:56 -0400

    Update Weapon.js

--- presents/Weapon.js
+++ presents/Weapon.js
@@ -1,13 +1,15 @@
 export default class Weapon {    
-    constructor (name="", numdice=0, ddie="", dtype="", hit=0, dmg=0){
+    constructor (name="", numdice=0, ddie=0, dtype="", hit=0, dmg=0){
         this.Name = name
         this.NumDice = numdice
         this.DamageDie = ddie        
         this.DamageType = dtype
         this.BonusToHit = hit
         this.BonusToDmg = dmg
-        this.WeaponString = "";
-        this.WeaponString2 = "";
+        if (typeof(this.NumDice) === "string") {            
+            this.WeaponString = numdice;
+            this.WeaponString2 = ddie;
+        }
     }
     
 //      constructor (name="", tohit=0, weapon="", weapon2="") {
