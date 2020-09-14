0d335f70c1aee8dac2db5505fa86d3e351282ae2
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-13 23:38:50 -0400

    Update web.js

--- web.js
+++ web.js
@@ -281,7 +281,7 @@ function toggleDetails(event, rollArray) {
                             }
                         }
                         else {
-                            detailAppend += `<span id="${mobTag}-Detail" style="margin-left:15px; color:#ffb5b5"> [${rollArray[i][j].hitRoll}] Miss`;
+                            detailAppend += `<span id="${mobTag}-Detail" style="margin-left:15px; color:#ff9191"> [${rollArray[i][j].hitRoll}] Miss`;
                         }
                         detailAppend += ` </span><br>`;
                     }
