a2978374281857e0b9a708e2d498c9ce25ba0ed5
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-21 15:46:31 -0400

    Update WeaponModMenu.js

--- templates/WeaponModMenu.js
+++ templates/WeaponModMenu.js
@@ -3,72 +3,22 @@ export function addWeaponMobTemplate(title, roll) {
   /// Add Modifier V
  return `
  <ul class="main-nav">
-  <li>Extra Damage Roll
-  </li>
-  <li>Attack Range
+  <li>Damage Roll</li>
+  <li>DC</li>
+  <li>Condition
     <ul>
-      <li>Melee within 5ft</li>
-      <li>Melee outside 5ft</li>
-      <li>Ranged</li>
+      <li>Knock Prone</li>
+      <li>Restrain</li>
+      <li>Paralyze</li>
     </ul>
   </li>
-  <li>Saving Throw
+  <li>Range
     <ul>
-      <li>Extra Damage
-        <ul>
-          <li>Half damage on a success</li>
-          <li>No damage on a success</li>
-        </ul>
-      </li>      
-      <li>Extra Damage With Condition
-        <ul>
-          <li>Knocks Prone
-            <ul>
-              <li>Half damage on a success</li>
-              <li>No damage on a success</li>
-            </ul>
-          </li>
-          <li>Paralyzes
-            <ul>
-              <li>Half damage on a success</li>
-              <li>No damage on a success</li>
-            </ul>
-          </li>
-          <li>Restrains
-            <ul>
-              <li>Half damage on a success</li>
-              <li>No damage on a success</li>
-            </ul>
-          </li>          
-        </ul>
-      </li>
-      <li>Condition
-        <ul>
-          <li>Knocks Prone</li>
-          <li>Paralyzes</li>
-          <li>Restrains</li>
-        </ul>
-      </li>
+      <li>Within 5ft</li>
+      <li>Outside of 5ft</li>
     </ul>
   </li>
-  
-  <li>Front End Design
-    <ul>
-      <li><a href="#">HTML</a></li>
-      <li><a href="#">CSS</a>
-        <ul>
-          <li><a href="#">Resets</a></li>
-          <li><a href="#">Grids</a></li>
-          <li><a href="#">Frameworks</a></li>
-        </ul>
-      </li>
-      <li><a href="#">JavaScript</a>
-        <ul>
-          <li><a href="#">Ajax</a></li>
-          <li><a href="#">jQuery</a></li>
-        </ul>
-      </li>
-    </ul>
-  </li>
+ </ul>
+ 
   `
 }
