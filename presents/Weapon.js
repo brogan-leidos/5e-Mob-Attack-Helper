6bab68864b9b6b5d2545778659da47c602cb746b
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-23 15:47:07 -0400

    Update Weapon.js

--- presents/Weapon.js
+++ presents/Weapon.js
@@ -3,6 +3,7 @@ export default class Weapon {
         this.Name = name;
         this.BonusToHit = args[0][1];
         this.WeaponString = args[1][1];
+        this.IsMelee = args[2][1];
         this.WeaponMods = args;
     }
        
