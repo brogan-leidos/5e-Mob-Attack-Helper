0b9402c9ae4487f2e1e98a90cc75766616cbc2e7
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-23 16:04:03 -0400

    Update utils.js

--- templates/utils.js
+++ templates/utils.js
@@ -3,7 +3,7 @@ import Weapon from '../presents/Weapon.js'
 // Converts weapons into html options
 export function weaponsToHtml(weapons) {
   // weapons is a weapons array
-  // Example of weapon: (Name, [["ToHit", 1], ["Weapon", "1d6 + 2 slashing"], ["DC", 10, "Str"], ["Condition", "Restrain"]])
+  // Example of weapon: (Name, [["ToHit", 1], ["Weapon", "1d6 + 2 slashing"], ["IsMelee", true], ["DC", 10, "Str"], ["Condition", "Restrain"]])
   
   var htmlReturn = "";
   for(var i=0; i < weapons.length; i++) {    
