30dffd0b3d3a42e2908c6be97f8ff675b698f5c5
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-23 08:58:40 -0400

    Update WeaponModMenu.js

--- templates/WeaponModMenu.js
+++ templates/WeaponModMenu.js
@@ -1,27 +1,3 @@
-export function addWeaponMobTemplate(title, roll) {  
-  
-  /// Add Modifier V
- return `
- <ul class="main-nav">
-  <li>Damage Roll</li>
-  <li>DC</li>
-  <li>Condition
-    <ul>
-      <li>Knock Prone</li>
-      <li>Restrain</li>
-      <li>Paralyze</li>
-    </ul>
-  </li>
-  <li>Range
-    <ul>
-      <li>Within 5ft</li>
-      <li>Outside of 5ft</li>
-    </ul>
-  </li>
- </ul>
- 
-  `
-}
 
 export function modifierRow(underDc) {    
  if (!underDc) {
