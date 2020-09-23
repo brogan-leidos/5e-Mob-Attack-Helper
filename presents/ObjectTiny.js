5f6b3862e1e545760079eb1cbefd1805a59ff777
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-23 15:42:08 -0400

    Update ObjectTiny.js

--- presents/ObjectTiny.js
+++ presents/ObjectTiny.js
@@ -18,7 +18,7 @@ export default class ObjectTiny extends Mob {
     this.DamageRoll = new DamageRoll();
     
     this.Weapons = [
-      new Weapon("Slam", 1, 4, "bludgeoning", 8, 4)
+      new Weapon("Slam", [["ToHit", 8], ["Weapon","1d4 + 4 bludgeoning"]]),
     ];
     
     this.EquipWeapon = this.Weapons[0];
