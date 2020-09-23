815ce8e7e84628bc1f13b3ce3028b61f8367e1eb
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-23 15:31:08 -0400

    Update utils.js

--- templates/utils.js
+++ templates/utils.js
@@ -1,23 +1,15 @@
 import Weapon from '../presents/Weapon.js'
 
-// Converts a weapon into an html option
+// Converts weapons into html options
 export function weaponsToHtml(weapons) {
   // weapons is a weapons array
+  // Example of weapon: (Name, [["ToHit", 1], ["Weapon", "1d6 + 2 slashing"], ["DC", 10, "Str"], ["Condition", "Restrain"]])
   
   var htmlReturn = "";
   for(var i=0; i < weapons.length; i++) {    
     var weapon = weapons[i];
-    var weaponString = "";
-    if (!weapon.WeaponString) {
-      weaponString = weapon.NumDice.toString() + "d" + weapon.DamageDie.toString() + " + " + 
-        weapon.BonusToDmg.toString() + " " + weapon.DamageType;
-    } 
-    else {
-      weaponString = weapon.WeaponString;
-    }
-
     htmlReturn += `
-      <option id="${weapon.Name}" value="ToHit:${weapon.BonusToHit.toString()} Weapon:${weaponString} Weapon2:${weapon.WeaponString2}">
+      <option id="${weapon.Name}" value="${weapon.WeaponMods}">
         ${weapon.Name}
       </option>
     `
