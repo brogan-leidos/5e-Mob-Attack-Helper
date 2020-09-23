bad09ecc22328ace55120abb54640d3db6bfbf1d
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-23 16:57:49 -0400

    Update utils.js

--- templates/utils.js
+++ templates/utils.js
@@ -6,11 +6,11 @@ export function weaponsToHtml(weapons) {
   // Example of weapon: (Name, [["ToHit", 1], ["Weapon", "1d6 + 2 slashing"], ["IsMelee", true], ["DC", 10, "Str"], ["Condition", "Restrain"]])
   
   var htmlReturn = "";
-  var stringify = JSON.stringify(weapon.WeaponMods);
-  stringify = stringify.replace(/\\/g, "");
+
   for(var i=0; i < weapons.length; i++) {    
     var weapon = weapons[i];
     var stringify = JSON.stringify(weapon.WeaponMods);
+    stringify = stringify.replace(/\\/g, "");
 
     htmlReturn += `
       <option id="${weapon.Name}" value=${stringify}>
