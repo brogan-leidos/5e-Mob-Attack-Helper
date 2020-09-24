c4709f21a056a7fbd11f67db008973c0ceada65f
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-24 09:51:23 -0400

    Update web.js

--- web.js
+++ web.js
@@ -189,7 +189,7 @@ function expandWeapon(mobTag) {
         document.getElementById(`${mobTag}-Weapon-Expand`).insertAdjacentHTML('beforebegin',`<span class="weaponCollapseButton" id="${mobTag}-Weapon-Collapse"><i class="fa fa-minus-square-o"></i></span>`);
         document.getElementById(`${mobTag}-Weapon-Expand-Tip`).style.display = "none";  
         document.getElementById(mobTag + "-Weapon-Collapse").addEventListener('click', (e) => {
-            collapseRow(e.target.id);
+            collapseRow(e.target.parentElement.id);
         });  
     }
     var underDc = checkIfUnderDc(mobTag, modRow);
