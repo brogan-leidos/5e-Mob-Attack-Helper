eca82f8cf470c3d4bd30b947765098ddf3f764a3
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-24 12:27:38 -0400

    Update DamageRoll.js

--- presents/DamageRoll.js
+++ presents/DamageRoll.js
@@ -3,6 +3,8 @@ export default class DamageRoll {
         this.crit = false;
         this.autoCrit = false;
         this.hitRoll = 0;
+        this.hitRoll2 = 0;
+        this.vantage = 0;
         this.attacker = null;
         this.damageResults = []; // Expects: [[damage, type]]
         this.missed = false;
