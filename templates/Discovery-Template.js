28c8122d92a5bcb5e07799e535a206056fdd7b75
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-24 08:45:13 -0400

    Update Discovery-Template.js

--- templates/Discovery-Template.js
+++ templates/Discovery-Template.js
@@ -23,3 +23,33 @@ export function discoveryTemplate(title, roll, option1="op1", option2="op2") {
   </table>
   `
 }
+
+
+export function dcTemplate(title, dc, roll, option1="op1", option2="op2") {  
+  
+ return `
+  <table class="dcBlock">
+    <tbody>
+      <tr>
+        <td colspan=2 style="border-bottom: thin solid black">${title}</td>
+      </tr>
+      <tr>
+        <td colspan=2 style="border-bottom: thin solid black">${dc}</td>
+      </tr>
+      <tr>
+        <td colspan=2>
+           ${roll}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <button class="hitButton" id="hitButton-${option1}">${option1}</button>
+        </td>
+        <td>
+          <button class="missButton" id="missButton-${option2}">${option2}</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  `
+}
