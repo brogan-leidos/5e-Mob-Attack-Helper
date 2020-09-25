154d2f7e6d3f35c8a39680992985c380bb908331
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-09-25 11:07:32 -0400

    Update web.js

--- web.js
+++ web.js
@@ -82,7 +82,7 @@ function toggleAdvancedOptions() {
     var advancedDiv = document.getElementById("advancedOptionsTab");
     advancedDiv.style.display = advancedDiv.style.display == "table" ? "none" : "table";
     var buttonIcon = document.getElementById("advancedOptionsButton").firstElementChild;
-    if (buttonIcon.classList.value.indcludes("fa-angle-double-down")) {
+    if (buttonIcon.classList.value.includes("fa-angle-double-down")) {
         buttonIcon.classList = "fa fa-angle-double-up";    
     }
     else {
