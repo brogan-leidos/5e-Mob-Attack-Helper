b8ca071d4f30289f24650bbd02bbd38a18588829
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-10-09 14:00:51 -0400

    Create Boar.js

---
+++ presents/Boar.js
@@ -0,0 +1,22 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
+
+export default class Boar extends Mob {
+  constructor() {
+    super();
+    this.Name = "Boar";
+    this.Icon = "\"boar\""
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Tusk", [["ToHit", 3],  ["IsMelee", true], ["Weapon","1d6 + 1 slashing"]]),
+      new Weapon("Tusk (Charge)", [["ToHit", 3],  ["IsMelee", true], ["Weapon","1d6 + 1 slashing"], ["Extra Damage", "1d6 + 0 slashing"], ["DC", 11, "Str"], ["Condition", "Knock Prone"]]),      
+    ];
+    
+    this.EquipWeapon = this.Weapons[1];
+    
+  }
+  
+}
