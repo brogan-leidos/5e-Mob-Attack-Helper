0124d5be20d8f23f227ffdf8fad817279d5bbaf2
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-10-09 14:10:17 -0400

    Create GiantSnake.js

---
+++ presents/GiantSnake.js
@@ -0,0 +1,21 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
+
+export default class GiantFrog extends Mob {
+  constructor() {
+    super();
+    this.Name = "GiantSnake";
+    this.Icon = "\"snake\""
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Bite", [["ToHit", 6],  ["IsMelee", false], ["Weapon","1d4 + 4 piercing"], ["DC", 13, "Con"]]),
+    ];
+    
+    this.EquipWeapon = this.Weapons[1];
+    
+  }
+  
+}
