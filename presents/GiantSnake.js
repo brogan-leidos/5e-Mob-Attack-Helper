55badc9c637e0ff12a7023cb31d662e4f370d6e0
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-10-09 14:11:38 -0400

    Update GiantSnake.js

--- presents/GiantSnake.js
+++ presents/GiantSnake.js
@@ -2,7 +2,7 @@ import Mob from './Mob.js'
 import Weapon from './Weapon.js'
 import DamageRoll from './DamageRoll.js'
 
-export default class GiantFrog extends Mob {
+export default class GiantSnake extends Mob {
   constructor() {
     super();
     this.Name = "GiantSnake";
@@ -11,7 +11,7 @@ export default class GiantFrog extends Mob {
     this.DamageRoll = new DamageRoll();
     
     this.Weapons = [
-      new Weapon("Bite", [["ToHit", 6],  ["IsMelee", false], ["Weapon","1d4 + 4 piercing"], ["DC", 13, "Con"]]),
+      new Weapon("Bite", [["ToHit", 6],  ["IsMelee", false], ["Weapon","1d4 + 4 piercing"], ["DC", 11, "Con"], ["Damage (1/2 on save)", "3d6 + 0 poison"]]),
     ];
     
     this.EquipWeapon = this.Weapons[0];
