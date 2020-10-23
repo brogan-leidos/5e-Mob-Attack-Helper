2c93b771fd73d396aad9da1f8bcae8b91eaf4b5d
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-10-23 12:48:19 -0400

    Update DamageRoll.js

--- presents/DamageRoll.js
+++ presents/DamageRoll.js
@@ -2,14 +2,18 @@ export default class DamageRoll {
     constructor(){
         this.crit = false;
         this.autoCrit = false;
+        this.missed = false;
+        
         this.hitRoll = 0;
         this.attackRoll1 = 0;
         this.attackRoll2 = 0;
-        this.vantage = 0;
-        this.attacker = null;
-        this.damageResults = []; // Expects: [[damage, type]]
-        this.missed = false;
-        this.message = "";
+        this.vantage = 0; // 1: Adv, -1: DisAdv
+        
+        this.attacker = null; // Points back to the spawned mob that made the attack
+        this.damageResults = []; // Expects: [[damage, type]], used to calculate total damage
+        this.rollBreakdown = []; // Expects: [1,2,...,n], used to show tooltips
+        
         this.error = false;
+        this.message = "";        
     }
 }
