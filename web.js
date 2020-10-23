1ee5895cee13337468e47353db0094761bd1880f
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-10-23 13:23:59 -0400

    Update web.js

--- web.js
+++ web.js
@@ -510,12 +510,12 @@ function toggleDetails(event, rollArray) {
                     var damageTotalsDisplay = "";
                     if (!rollClass.missed) {
                         for (var dmg=0; dmg < rollClass.damageResults.length; dmg++) {
-                            damageTotalsDisplay += `<div class="damageRoll"><span id="${rollClass.attacker.Name}-${rollClass.attacker.Number}-DamageRoll-${dmg}"> 
+                            damageTotalsDisplay += `<span class="damageRoll"><span id="${rollClass.attacker.Name}-${rollClass.attacker.Number}-DamageRoll-${dmg}"> 
                                                       ⚔${rollClass.damageResults[dmg][0]}
                                                     </span>
-                                                    <div class="damageRollTip" id="${rollClass.attacker.Name}-${rollClass.attacker.Number}-DamageRoll-${dmg}-Details">
+                                                    <span class="damageRollTip" id="${rollClass.attacker.Name}-${rollClass.attacker.Number}-DamageRoll-${dmg}-Details">
                                                       ${rollClass.rollBreakdown[dmg]}
-                                                    </div></div>`;
+                                                    </span></span>`;
                                  
                         }
                             
