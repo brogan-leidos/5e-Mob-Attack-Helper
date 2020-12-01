b4bcb41d09f18c9776b7acf0a92b3d70d492c95a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-12-01 16:48:21 -0500

    Update web.js

--- web.js
+++ web.js
@@ -319,7 +319,7 @@ function expandWeapon(mobTag) {
 }
 
 function getNumModRows(mobTag) {
-    return document.getElementById(mobTag).children[1].firstElementChild.children.length - 7;
+    return document.getElementById(mobTag).children[1].firstElementChild.children.length - 8;
 }
 
 // Not created by user, used to change present weapons
