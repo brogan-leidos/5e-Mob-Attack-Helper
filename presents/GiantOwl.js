3bcd9b2815589a026b5ebfa198e1cef963dca66a
Andrew Brogan <andrew.brogan@pfasolutions.com>
2021-04-24 16:38:32 -0400

    asdf

---
+++ presents/GiantOwl.js
@@ -0,0 +1,21 @@
+import Mob from './Mob.js'
+import Weapon from './Weapon.js'
+import DamageRoll from './DamageRoll.js'
+
+export default class GiantOwl extends Mob {
+  constructor() {
+    super();
+    this.Name = "Giant Owl";
+    this.Icon = "\"owl\""
+    
+    this.DamageRoll = new DamageRoll();
+    
+    this.Weapons = [
+      new Weapon("Talons", [["ToHit", 3],  ["IsMelee", true], ["Weapon","2d6 + 1 slashing"]]),
+    ];
+    
+    this.EquipWeapon = this.Weapons[1];
+    
+  }
+  
+}
