a41d17950f746307e4f3a392f69c86880357f839
Andrew Brogan <andrew.brogan@pfasolutions.com>
2021-04-28 00:55:15 -0400

    bugs and yugs

--- presents/ConstrictSnake.js
+++ presents/ConstrictSnake.js
@@ -15,7 +15,7 @@ export default class ConstrictSnake extends Mob {
       new Weapon("Constrict", [["ToHit", 4],  ["IsMelee", true], ["Weapon","1d8 + 2 bludgeoning"], ["Condition", "Grappled"], ["Condition", "Restrain"]]),
     ];
     
-    this.EquipWeapon = this.Weapons[0];
+    this.EquipWeapon = this.Weapons[1];
     
     this.Variants = ["Poisonous Snake", "Giant Poisonous Snake", "Constrictor Snake", "Flying Snake"];
 
