796a785502eaa84610432f40ddf9ebdec6c6a600
Andrew Brogan <andrew.brogan@pfasolutions.com>
2021-09-10 22:07:21 -0400

    Cleanups and bugs

--- web.js
+++ web.js
@@ -1344,7 +1344,7 @@ function parseMobs(numBlocks) {
         var number = document.getElementById(`${mobTag}-Number`).value;
         for (var j=0; j < number; j++) {            
             newMob.Number = j+1;
-            var newObjectMob = Object.create(Mob);
+            var newObjectMob = new Mob();
             Object.assign(newObjectMob, newMob);
             mobArray[i].push(newObjectMob); 
         };
