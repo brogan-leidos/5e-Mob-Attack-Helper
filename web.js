64cae70a797185454c8936d52247ebf8d40fe8e1
Andrew Brogan <andrew.brogan@pfasolutions.com>
2021-09-24 20:06:12 -0400

    Cleanups and bugs

--- web.js
+++ web.js
@@ -914,7 +914,7 @@ function toggleDetails(event, rollArray) {
                 var creatureColumn = ""
                 if (rollClass.attacker.Multiattack) {
                     if (j % rollClass.attacker.Multiattack.length == 0) {
-                        creatureColumn = `<td rowspan="${rollClass.attacker.Multiattack}"><span class="creatureGroup">${rollClass.attacker.Number}</span></td>`;
+                        creatureColumn = `<td rowspan="${rollClass.attacker.Multiattack.length}"><span class="creatureGroup">${rollClass.attacker.Number}</span></td>`;
                     }
                 }
 
