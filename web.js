95b2a26730b5b26342d2a484194dc7ec0dcafea4
Andrew Brogan <andrew.brogan@pfasolutions.com>
2022-07-09 19:24:45 -0400

    asdf

--- web.js
+++ web.js
@@ -105,7 +105,7 @@ function fetchMonsterInfo(value) {
                     var name = action.name;
                     var bonusToHit = entry.match(/{@hit (\d+)/)[1];
                     var damage = entry.match(/{@damage ([^}]+)}/)[1];
-                    var damageType = entry.match(/{@damage [^}]+}\) (\w) damage/)[1]
+                    var damageType = entry.match(/{@damage [^}]+}\) (\w+) damage/)[1]
                     var isMelee = entry.search(/{[^}]*mw[^}]*}/) !== -1;
                     var weaponString = `${damage} ${damageType}}`
                     console.log(newWeapon);
