c9e2c51f22ba803244197949c39370f39369ec13
Andrew Brogan <andrew.brogan@pfasolutions.com>
2022-07-29 02:04:58 -0400

    asdf

--- web.js
+++ web.js
@@ -311,7 +311,7 @@ function setStatBlockEventListeners(mobTag, monsterName, actions) {
     document.getElementById('statBlockCloseButton').addEventListener('click', () => {
         document.getElementsByClassName('statBlockContainer')[0].animate([
             {transform: 'scale(0.5)', opacity: '0'}
-        ], {duration: 300})
+        ], {duration: 300, fill: 'forwards'})
         setTimeout(() => {
             for (let block of blockArray) {
                 document.getElementById(block).classList.remove('highlighted');
